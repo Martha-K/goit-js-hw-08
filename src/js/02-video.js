8fa836e5b2dac5d117953763a20616a3739662c8
Martha-K <kozenyukmarta@gmail.com>
2023-03-23 17:13:56 +0200

    the first and second tasks are completed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,11 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-// player.on('play', function () {
-//   console.log('played the video!');
-// });
-const videoPlaybackTime = player.on('timeupdate', function () {
-    console.log('hdgch')
-});
-localStorage.setItem('videoplayer-current-time', videoPlaybackTime);
-// player.getVideoTitle().then(function (title) {
-//   console.log('title:', title);
-// });
+player.on('timeupdate', throttle(playerTime, 1000));
+
+function playerTime() {
+    localStorage.setItem('videoplayer-current-time', data);
+}
