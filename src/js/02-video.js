cb3ae6280e4c01f59960396430722d7d74670f32
Martha-K <kozenyukmarta@gmail.com>
2023-03-24 22:00:38 +0200

    corrected the second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,11 +5,13 @@ const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
 player.on('timeupdate', throttle(playerTime, 1000));
-
+// player.setCurrentTime(currentTime);
 function playerTime(time) {
   localStorage.setItem('videoplayer-current-time', time.seconds);
 }
-
+// player.on("seeked", function() {
+//     player.setCurrentTime(currentTime);
+// })
 function onPlay() {
   const currentTime = localStorage.getItem('videoplayer-current-time');
   if (currentTime) {
@@ -17,3 +19,9 @@ function onPlay() {
   }
 }
 player.on('play', onPlay);
+onPlay();
+player.on('ready', () => {
+  // const currentTime = localStorage.getItem('videoplayer-current-time');
+  // player.setCurrentTime(currentTime);
+  console.log('ddd');
+});
